13887a83c06c8e89434028a07e18dabd0fc2a2b5
Dimitar Petrov <dimitar.petrov.311@gmail.com>
2017-11-23 21:50:28 +0200

    Assignment

---
+++ MultArrays/MultidimentionalArrays/04.LargestAreaInMatrix/LargestAreaInMatrix.cs
@@ -0,0 +1,131 @@
+﻿using System;
+using System.Collections.Generic;
+
+class LargestAreaInMatrix
+{
+    static void Main(string[] args)
+    {
+        string input = Console.ReadLine();
+        int n = int.Parse(input.Split(' ')[0]);
+        int m = int.Parse(input.Split(' ')[1]);
+        string[,] matrix = new string[n, m];
+        bool[,] visited = new bool[n,m]; // Visited Elements
+        for (int row = 0; row < n; row++)
+        {
+            string[] inputRow = (Console.ReadLine()).Split(' ');
+            for (int col = 0; col < m; col++)
+            {
+                matrix[row, col] = inputRow[col];
+            }
+        }
+        //Read Matrix
+
+        List<int> stackRows = new List<int>();
+        List<int> stackCols = new List<int>();
+
+        int maxAreaEquals = 1;
+
+
+        for (int r = 0; r < n; r++)
+        {
+            for (int c = 0; c < m; c++)
+            {
+                if (!visited[r, c])
+                {
+                    visited[r, c] = true;
+                    stackRows.Add(r);
+                    stackCols.Add(c);
+
+                    int curR = r;
+                    int curC = c;
+                    string currentEl = matrix[curR, curC];
+                    bool hasEquals = true;
+
+                    int currentMax = 1;
+
+
+                    while (hasEquals)
+                    {
+                        if ((curC + 1 < m) && (currentEl == matrix[curR, curC + 1]) && !visited[curR, curC + 1])
+                        {
+                            curC++;
+
+                            visited[curR, curC] = true;
+                            currentMax++;
+
+                            stackRows.Add(curR);
+                            stackCols.Add(curC);
+
+                            continue;
+                        }
+                        //Check Equal Right
+
+                        if ((curR + 1 < n) && (currentEl == matrix[curR + 1, curC]) && !visited[curR + 1, curC])
+                        {
+                            curR++;
+
+                            visited[curR, curC] = true;
+                            currentMax++;
+
+                            stackRows.Add(curR);
+                            stackCols.Add(curC);
+                            
+                            continue;
+                        }
+                        //Check Equal Down
+
+                        if ((curC - 1 >= 0) && (currentEl == matrix[curR, curC - 1]) && !visited[curR, curC - 1])
+                        {
+                            curC--;
+
+                            visited[curR, curC] = true;
+                            currentMax++;
+
+                            stackRows.Add(curR);
+                            stackCols.Add(curC);
+
+                            continue;
+                        }
+                        //Check Equal Left
+
+                        if ((curR - 1 >= 0) && (currentEl == matrix[curR - 1, curC]) && !visited[curR - 1, curC])
+                        {
+                            curR--;
+
+                            visited[curR, curC] = true;
+                            currentMax++;
+
+                            stackRows.Add(curR);
+                            stackCols.Add(curC);
+
+                            continue;
+                        }
+                        //Check Equal Up
+
+
+                        stackRows.RemoveAt(stackRows.Count - 1);
+                        stackCols.RemoveAt(stackCols.Count - 1);
+
+                        if (stackRows.Count != 0)
+                        {
+                            curR = stackRows[stackRows.Count - 1];
+                            curC = stackCols[stackCols.Count - 1];
+                        }
+                        else
+                        {
+                            hasEquals = false;
+                        }
+                    }
+
+                    if (currentMax > maxAreaEquals)
+                    {
+                        maxAreaEquals = currentMax;
+                    }
+                }
+            }
+        }
+
+        Console.WriteLine(maxAreaEquals);
+    }
+}
+
--- MultArrays/MultidimentionalArrays/04.LargestAreaInMatrix/Properties/AssemblyInfo.cs
+++ MultArrays/MultidimentionalArrays/04.LargestAreaInMatrix/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("test")]
+[assembly: AssemblyTitle("04.LargestAreaInMatrix")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("test")]
+[assembly: AssemblyProduct("04.LargestAreaInMatrix")]
 [assembly: AssemblyCopyright("Copyright ©  2017")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
@@ -20,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("070e158e-4bdd-4e70-ad28-ab0531998664")]
+[assembly: Guid("b7b607af-2403-4e8d-b2e2-7609c3e87da7")]
 
 // Version information for an assembly consists of the following four values:
 //
